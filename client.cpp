bb891e188abc1ceb2dd7f87827c4025f02df0965
Tor E Hagemann <hagemt@rpi.edu>
2012-02-04 20:32:51 -0500

    Did some initial interface building
    Sorry about the lack of hard tabs.
    I noticed QtCreator does some silly stuff, so I'll work on putting
    things to rights. The interface design is very basic, but I made
    some common options available. None of the functionality is hooked
    together, unfortunately. Qt is taking some gettings used to.

--- client.cpp
+++ client.cpp
@@ -1,19 +1,19 @@
 #include "client.h"
 #include "ui_client.h"
 
-Client::Client(QWidget *parent) :
+LirchQtInterface::LirchQtInterface(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Client)
 {
     ui->setupUi(this);
 }
 
-Client::~Client()
+LirchQtInterface::~LirchQtInterface()
 {
     delete ui;
 }
 
-void Client::changeEvent(QEvent *e)
+void LirchQtInterface::changeEvent(QEvent *e)
 {
     QMainWindow::changeEvent(e);
     switch (e->type()) {
@@ -23,4 +23,19 @@ void Client::changeEvent(QEvent *e)
     default:
         break;
     }
+}
+
+void LirchQtInterface::on_msgSendButton_clicked()
+{
+    QMessageBox::warning(this,
+                         tr("Unimplemented Feature"),
+                         tr("'%1' could not be sent.").arg("[use ui to fetch msgTextArea.getText()]"),
+                         QMessageBox::Ok);
+}
+
+void LirchQtInterface::on_actionAbout_triggered()
+{
+    QMessageBox::information(this,
+                             tr("About Lirch %1").arg(LIRCH_VERSION_STRING),
+                             tr("// TODO insert Copyright and License info"));
 }
