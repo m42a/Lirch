48f7084d3d5a976284fcf36f006fa7160cab2179
Marc Aldorasi <m101010a@gmail.com>
2012-02-05 22:53:01 -0500

    Added more tests

--- message_test.cpp
+++ message_test.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const string tests[]={"toplugin", "tocore", "readempty"};
+const string tests[]={"toplugin", "tocore", "readempty", "cycle", "order"};
 
 int dotest(string test)
 {
@@ -30,6 +30,28 @@ int dotest(string test)
 		if (mp.plugin_read().text!="")
 			return 1;
 		if (mp.core_read().text!="")
+			return 1;
+		return 0;
+	}
+	if (test=="cycle")
+	{
+		mp.plugin_write(message{"Whee!"});
+		mp.core_write(mp.core_read());
+		mp.plugin_write(mp.plugin_read());
+		if (mp.core_read().text!="Whee!")
+			return 1;
+		return 0;
+	}
+	if (test=="order")
+	{
+		mp.plugin_write(message{"Message 1"});
+		mp.core_write(message{"Message 2"});
+		mp.plugin_write(message{"Message 3"});
+		if (mp.core_read().text!="Message 1")
+			return 1;
+		if (mp.plugin_read().text!="Message 2")
+			return 1;
+		if (mp.core_read().text!="Message 3")
 			return 1;
 		return 0;
 	}
@@ -66,7 +88,9 @@ int main(int argc, char *argv[])
 	int ret=dotest(argv[n]);
 	if (verbose)
 	{
-		if (ret==1)
+		if (ret==0)
+			cout << "Test " << argv[n] << " was successful\n";
+		else if (ret==1)
 			cout << "Test " << argv[n] << " failed\n";
 		else if (ret==2)
 			cout << "Unsupported test " << argv[n] << endl;
