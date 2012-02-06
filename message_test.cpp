c79e1311e1a129f3971093e85eb14083bf8c3b47
Marc Aldorasi <m101010a@gmail.com>
2012-02-05 22:36:30 -0500

    Added a test and testing options

--- message_test.cpp
+++ message_test.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+const string tests[]={"toplugin", "tocore", "readempty"};
+
 int dotest(string test)
 {
 	message_pipe mp;
@@ -23,16 +25,51 @@ int dotest(string test)
 			return 1;
 		return 0;
 	}
+	if (test=="readempty")
+	{
+		if (mp.plugin_read().text!="")
+			return 1;
+		if (mp.core_read().text!="")
+			return 1;
+		return 0;
+	}
+	return 2;
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc<=2)
+	if (argc<2)
 		return 0;
-	int ret=dotest(argv[1]);
-	if (ret==1)
-		cout << "Test " << argv[1] << " failed\n";
-	else if (ret==2)
-		cout << "Unsupported test " << argv[1] << endl;
+	bool verbose=false;
+	int n=1;
+	while (argv[n]!=NULL && argv[n][0]=='-')
+	{
+		for (int o=1;argv[n][o]!='\0';++o)
+		{
+			if (argv[n][o]=='v')
+				verbose=true;
+			else if (argv[n][o]=='q')
+				verbose=false;
+			else
+			{
+				cout << "Unsupported option '" << argv[n][o] << "'\n";
+				return 3;
+			}
+		}
+		++n;
+	}
+	if (argv[n]==NULL)
+	{
+		cout << "No test specified\n";
+		return 3;
+	}
+	int ret=dotest(argv[n]);
+	if (verbose)
+	{
+		if (ret==1)
+			cout << "Test " << argv[n] << " failed\n";
+		else if (ret==2)
+			cout << "Unsupported test " << argv[n] << endl;
+	}
 	return ret;
 }
