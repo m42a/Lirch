a482f176821fb5d57a95e9cd605b244e61de7f65
Marc Aldorasi <m101010a@gmail.com>
2012-03-09 19:15:41 -0500

    Gave messages a type and priority

--- message_test.cpp
+++ message_test.cpp
@@ -14,22 +14,22 @@ int dotest(string test)
 	if (test=="toplugin")
 	{
 		mp.core_write(message{"Hello, plugin"});
-		if (mp.plugin_read().text!="Hello, plugin")
+		if (mp.plugin_read().type!="Hello, plugin")
 			return 1;
 		return 0;
 	}
 	if (test=="tocore")
 	{
 		mp.plugin_write(message{"Hello, core"});
-		if (mp.core_read().text!="Hello, core")
+		if (mp.core_read().type!="Hello, core")
 			return 1;
 		return 0;
 	}
 	if (test=="readempty")
 	{
-		if (mp.plugin_read().text!="")
+		if (mp.plugin_read().type!="")
 			return 1;
-		if (mp.core_read().text!="")
+		if (mp.core_read().type!="")
 			return 1;
 		return 0;
 	}
@@ -38,7 +38,7 @@ int dotest(string test)
 		mp.plugin_write(message{"Whee!"});
 		mp.core_write(mp.core_read());
 		mp.plugin_write(mp.plugin_read());
-		if (mp.core_read().text!="Whee!")
+		if (mp.core_read().type!="Whee!")
 			return 1;
 		return 0;
 	}
@@ -47,11 +47,11 @@ int dotest(string test)
 		mp.plugin_write(message{"Message 1"});
 		mp.core_write(message{"Message 2"});
 		mp.plugin_write(message{"Message 3"});
-		if (mp.core_read().text!="Message 1")
+		if (mp.core_read().type!="Message 1")
 			return 1;
-		if (mp.plugin_read().text!="Message 2")
+		if (mp.plugin_read().type!="Message 2")
 			return 1;
-		if (mp.core_read().text!="Message 3")
+		if (mp.core_read().type!="Message 3")
 			return 1;
 		return 0;
 	}
@@ -59,21 +59,21 @@ int dotest(string test)
 	{
 		mp.plugin_write(message{"open the pod bay doors hal"});
 		message_pipe mp2=mp;
-		if (mp2.core_read().text!="open the pod bay doors hal")
+		if (mp2.core_read().type!="open the pod bay doors hal")
 			return 1;
 		cout << "part1\n";
 		mp2.core_write(message{"I'm sorry Dave"});
 		mp.core_write(message{"I'm afraid I can't do that"});
-		if (mp2.plugin_read().text!="I'm sorry Dave")
+		if (mp2.plugin_read().type!="I'm sorry Dave")
 			return 1;
 		message_pipe mp3=mp2;
-		if (mp3.plugin_read().text!="I'm afraid I can't do that")
+		if (mp3.plugin_read().type!="I'm afraid I can't do that")
 			return 1;
 		mp2.plugin_write(message{"whats the problem"});
-		if (mp3.core_read().text!="whats the problem")
+		if (mp3.core_read().type!="whats the problem")
 			return 1;
 		mp3.core_write(message{"Your grammar is too terrible, Dave"});
-		if (mp.plugin_read().text!="Your grammar is too terrible, Dave")
+		if (mp.plugin_read().type!="Your grammar is too terrible, Dave")
 			return 1;
 		return 0;
 	}
