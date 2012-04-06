82d93bd62fc62f6735d05be8ae76aeccb725a04b
Marc Aldorasi <m101010a@gmail.com>
2012-04-06 16:59:22 -0400

    Completely overhualed the meatgrinder

---
+++ plugins/grinder_messages.h
@@ -0,0 +1,36 @@
+#ifndef GRINDER_MESSAGES_H_
+#define GRINDER_MESSAGES_H_
+
+#include <functional>
+
+#include <QtCore/QString>
+#include <QtCore/QRegExp>
+
+#include "core/message.h"
+
+class register_replacer : public message_data
+{
+public:
+	virtual std::unique_ptr<message_data> copy() const {return std::unique_ptr<message_data>(new register_replacer(*this));}
+	static message create(const QString &c, const QRegExp &p, const QString &r) {return message_create("register_replacer", new register_replacer(c,p,r));}
+
+	register_replacer(const QString &c, const QRegExp &p, const QString &r) : command(c), pattern(p), replacement(r) {}
+
+	QString command;
+	QRegExp pattern;
+	QString replacement;
+};
+
+class register_handler : public message_data
+{
+public:
+	virtual std::unique_ptr<message_data> copy() const {return std::unique_ptr<message_data>(new register_handler(*this));}
+	static message create(const QString &c, const std::function<message (QString, QString)> &h) {return message_create("register_handler", new register_handler(c,h));}
+
+	register_handler(const QString &c, const std::function<message (QString, QString)> &h) : command(c), handler(h) {}
+
+	QString command;
+	std::function<message (QString, QString)> handler;
+};
+
+#endif
--- plugins/meatgrinder.cpp
+++ plugins/meatgrinder.cpp
@@ -1,13 +1,68 @@
+#include <unordered_map>
+
+#include <QtCore/QString>
+
 #include "lirch_plugin.h"
 #include "edict_messages.h"
+#include "grinder_messages.h"
 #include "core/core_messages.h"
 
 using namespace std;
 
+namespace std
+{
+	template <>
+	struct hash<QString>
+	{
+		size_t operator()(const QString& v) const
+		{
+			return std::hash<std::string>()(v.toStdString());
+		}
+	};
+}
+
+QString prefix(const QString &s)
+{
+	if (s.isEmpty() || s[0]!='/')
+		return "";
+	return s.left(s.indexOf(' '));
+}
+
+QString run_replacers(const unordered_multimap<QString, pair<QRegExp, QString>> &replacements, QString message)
+{
+	auto range=replacements.equal_range(prefix(message));
+	for (auto &i=range.first; i!=range.second; ++i)
+		message.replace(i->second.first, i->second.second);
+	return message;
+}
+
+message handle_me(QString text, QString channel)
+{
+	if (text=="/me")
+		return empty_message::create();
+	//Remove the leading "/me "
+	return me_edict_message::create(text.remove(0,4), channel);
+}
+
+message handle_quit(QString, QString)
+{
+	shutdown_message::create();
+}
+
+message handle_normal(QString text, QString channel)
+{
+	if (text.startsWith("/say"))
+		text.remove(0,5);
+	return edict_message::create(text, channel);
+}
+
 void run(plugin_pipe p, string name)
 {
-	//int pri=32000;
-	p.write(registration_message::create(32000, name, "raw_edict"));
+	p.write(registration_message::create(-30000, name, "raw_edict"));
+	p.write(registration_message::create(-30000, name, "register_replacer"));
+	p.write(registration_message::create(-30000, name, "register_handler"));
+	unordered_multimap<QString, pair<QRegExp, QString>> text_replacements;
+	unordered_map<QString, function<message (QString, QString)>> handlers;
 	while (true)
 	{
 		message m=p.blocking_read();
@@ -22,8 +77,36 @@ void run(plugin_pipe p, string name)
 			if (!s)
 				continue;
 			//Retry 2000 times until we succeed
-			if (!s->status && s->priority>30000)
-				p.write(registration_message::create(s->priority-1, name, s->type));
+			if (!s->status)
+			{
+				if (s->priority>-32000)
+					p.write(registration_message::create(s->priority-1, name, s->type));
+			}
+			else
+			{
+				if (s->type=="register_handler")
+				{
+					p.write(register_handler::create("", handle_normal));
+					p.write(register_handler::create("/say", handle_normal));
+					p.write(register_handler::create("/me", handle_me));
+					p.write(register_handler::create("/q", handle_quit));
+					p.write(register_handler::create("/quit", handle_quit));
+				}
+			}
+		}
+		else if (m.type=="register_replacer")
+		{
+			auto i=dynamic_cast<register_replacer *>(m.getdata());
+			if (!i)
+				continue;
+			text_replacements.insert({i->command, {i->pattern, i->replacement}});
+		}
+		else if (m.type=="register_handler")
+		{
+			auto i=dynamic_cast<register_handler *>(m.getdata());
+			if (!i)
+				continue;
+			handlers.insert({i->command, i->handler});
 		}
 		else if (m.type=="raw_edict")
 		{
@@ -34,15 +117,22 @@ void run(plugin_pipe p, string name)
 			if (str.isEmpty())
 				//We don't propagate empty messages (on principle)
 				continue;
-			if (str[0]!='/')
-				p.write(edict_message::create(str,e->channel));
-			else
+			auto pre=prefix(str);
+			auto mod=run_replacers(text_replacements, str);
+			if (prefix(mod)!=pre)
 			{
-				if (str.startsWith("/me "))
-					p.write(me_edict_message::create(str.section(' ',1),e->channel));
-				if (str=="/quit" || str.startsWith("/quit "))
-					p.write(core_quit_message::create());
+				//Recurse if the type of the message has been changed
+				p.write(raw_edict_message::create(mod, e->channel));
+				continue;
 			}
+			if (handlers.count(pre)==0)
+				//Nothing can handle this type of message, so complain
+				//We should be using tr here since this is a
+				//message to be displayed, but I'm not sure
+				//which tr to use.
+				p.write(information_display_message::create(e->channel, QString("Unknown message type \"%1\"").arg(pre)));
+			else
+				p.write(handlers[pre](str, e->channel));
 		}
 		else
 		{
