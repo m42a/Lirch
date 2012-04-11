eba6396d51695518cfcd34ac81ea4a0cd5a911ed
Marc Aldorasi <m101010a@gmail.com>
2012-04-11 19:52:11 -0400

    The basic UI can now display a single channel at a time

--- ui/basic/basic.cpp
+++ ui/basic/basic.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <thread>
 
+#include <QString>
+
 #include "plugins/lirch_plugin.h"
 #include "plugins/edict_messages.h"
 #include "plugins/display_messages.h"
+#include "plugins/grinder_messages.h"
 #include "plugins/channel_messages.h"
 #include "core/core_messages.h"
 
@@ -33,12 +36,35 @@ void send_input(plugin_pipe out, plugin_pipe in)
 	out.write(core_quit_message::create());
 }
 
+class display_single_channel : public message_data
+{
+public:
+	virtual std::unique_ptr<message_data> copy() const {return std::unique_ptr<message_data>(new display_single_channel(*this));}
+
+	//notify messages have only one conent, string to be displayed
+	static message create(const QString &chan) {return message_create("only", new display_single_channel(chan));}
+
+	display_single_channel(const QString &chan) : channel(chan) {}
+
+	QString channel;
+};
+
+message handle_only(QString text, QString)
+{
+	if (text.startsWith("/only"))
+		return display_single_channel::create(text.remove(0, 6));
+	return empty_message::create();
+}
+
 void run(plugin_pipe p, string name)
 {
+	QString channel="";
 	bidirectional_message_pipe bmp;
 	thread(send_input, p, plugin_pipe(bmp)).detach();
 	p.write(registration_message::create(-30000, name, "display"));
 	p.write(registration_message::create(-30000, name, "set_channel"));
+	p.write(registration_message::create(-30000, name, "handler_ready"));
+	p.write(registration_message::create(-30000, name, "only"));
 	while (true)
 	{
 		message m=p.blocking_read();
@@ -48,7 +74,19 @@ void run(plugin_pipe p, string name)
 		}
 		else if (m.type=="registration_status")
 		{
-			//Handle this
+			auto s=dynamic_cast<registration_status *>(m.getdata());
+			if (!s)
+				continue;
+			if (!s->status)
+			{
+				if (s->priority>-32000)
+					p.write(registration_message::create(s->priority-1, name, s->type));
+			}
+			else
+			{
+				if (s->type=="only")
+					p.write(register_handler::create("/only", handle_only));
+			}
 		}
 		else if (m.type=="display")
 		{
@@ -56,6 +94,8 @@ void run(plugin_pipe p, string name)
 			if (!s)
 				continue;
 			p.write(m.decrement_priority());
+			if (channel!="" && channel!=s->channel)
+				continue;
 
 			string channel=s->channel.toLocal8Bit().constData();
 			string nick=s->nick.toLocal8Bit().constData();
@@ -75,6 +115,17 @@ void run(plugin_pipe p, string name)
 				continue;
 			bmp.core_write(m);
 			p.write(m.decrement_priority());
+		}
+		else if (m.type=="only")
+		{
+			auto i=dynamic_cast<display_single_channel *>(m.getdata());
+			if (!i)
+				continue;
+			channel=i->channel;
+		}
+		else if (m.type=="handler_ready")
+		{
+			p.write(register_handler::create("/only", handle_only));
 		}
 		else
 		{
