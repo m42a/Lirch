e1ec570d33d66a4b6f8ddd9b12ed3178fbae95d3
Marc Aldorasi <m101010a@gmail.com>
2012-04-11 18:55:07 -0400

    UI can now change channels

--- ui/basic/basic.cpp
+++ ui/basic/basic.cpp
@@ -4,27 +4,41 @@
 #include "plugins/lirch_plugin.h"
 #include "plugins/edict_messages.h"
 #include "plugins/display_messages.h"
+#include "plugins/channel_messages.h"
 #include "core/core_messages.h"
 
 using namespace std;
 
-void send_input(plugin_pipe p)
+void send_input(plugin_pipe out, plugin_pipe in)
 {
+	QString channel="default";
 	string line;
 	while (getline(cin, line))
 	{
 		//I can't use the regular constructor since that assumes the
 		//locale is ASCII
 		QString q=QString::fromLocal8Bit(line.c_str());
-		p.write(raw_edict_message::create(q,"default"));
+		if (in.has_message())
+		{
+			message m=in.read();
+			if (m.type=="set_channel")
+			{
+				auto i=dynamic_cast<set_channel *>(m.getdata());
+				if (i)
+					channel=i->channel;
+			}
+		}
+		out.write(raw_edict_message::create(q,channel));
 	}
-	p.write(core_quit_message::create());
+	out.write(core_quit_message::create());
 }
 
 void run(plugin_pipe p, string name)
 {
-	thread(send_input, p).detach();
-	p.write(registration_message::create(-32000, name, "display"));
+	bidirectional_message_pipe bmp;
+	thread(send_input, p, plugin_pipe(bmp)).detach();
+	p.write(registration_message::create(-30000, name, "display"));
+	p.write(registration_message::create(-30000, name, "set_channel"));
 	while (true)
 	{
 		message m=p.blocking_read();
@@ -53,6 +67,14 @@ void run(plugin_pipe p, string name)
 				cout << channel << ": * " << nick << " " << contents << endl;
 			if(s->subtype==display_message_subtype::NOTIFY)
 				cout << channel << ": ‼‽ " << contents << endl;
+		}
+		else if (m.type=="set_channel")
+		{
+			auto i=dynamic_cast<set_channel *>(m.getdata());
+			if (!i)
+				continue;
+			bmp.core_write(m);
+			p.write(m.decrement_priority());
 		}
 		else
 		{
