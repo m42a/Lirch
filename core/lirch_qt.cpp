d630955cee4c5b09acd8a56ad0a79437512e0411
Tor E Hagemann <hagemt@rpi.edu>
2012-04-14 12:17:38 -0400

    Added TCLAP for argument parsing (preliminary)

--- core/lirch_qt.cpp
+++ core/lirch_qt.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 #include <QApplication>
 #include <QObject>
+#include <QString>
 
 #include "lirch_constants.h"
 #include "core/core_messages.h"
@@ -17,6 +18,46 @@ LirchClientPipe mediator;
 void run_core(const vector<message> &vm);
 
 extern bool verbose;
+
+#define USE_TCLAP
+#ifdef USE_TCLAP
+#include <tclap/CmdLine.h>
+
+static struct lirch_options {
+  bool list_preloads, no_preloads, verbose;
+  vector<string> plugins;
+} session;
+
+void parse_args(int argc, char *argv[]) {
+	try {
+		// Init the options object
+		QString id = QObject::tr("%1 %2 core (%3)").arg(
+						LIRCH_PRODUCT_NAME,
+						LIRCH_VERSION_STRING,
+						LIRCH_BUILD_HASH);
+		TCLAP::CmdLine options(id.toStdString(), ' ', LIRCH_VERSION_STRING);
+		// Specify switches TODO functionalize?
+		TCLAP::SwitchArg verboseSwitch("v", "verbose",
+			"Enable loud output (all messages)", options, false);
+		TCLAP::SwitchArg noPreloadsSwitch("n", "no_preloads",
+			"Don't load the preloaded plugins", options, false);
+		TCLAP::SwitchArg listPreloadsSwitch("l", "list_preloads",
+			"List the preloaded plugins", options, false);
+		TCLAP::UnlabeledMultiArg<string> plugin_pairs("plugin_pairs",
+			"Bare strings, in pairs like: antenna lib/libantenna.so", false,
+			"plugin_name plugin_file", "pair<string, string>");
+                options.add(plugin_pairs);
+		// Parse all the options
+		options.parse(argc, argv);
+		session.verbose = verboseSwitch.getValue();
+		session.no_preloads = noPreloadsSwitch.getValue();
+		session.list_preloads = listPreloadsSwitch.getValue();
+		session.plugins = plugin_pairs.getValue();
+	} catch (TCLAP::ArgException &e) {
+		cerr << e.error() << endl;
+	}
+}
+#endif // USE_TCLAP
 
 int main(int argc, char *argv[])
 {
@@ -34,43 +75,60 @@ int main(int argc, char *argv[])
         QObject::connect(&mediator,    SIGNAL(run(LirchClientPipe *)),
                          &main_window, SLOT(use(LirchClientPipe *)));
 
-	vector<message> vm;
-	// Preload a variety of plugins specified in build (see lirch_constants.h)
+	vector<pair<string, string>> plugins;
+	// Prepare the list of plugins specified in build (see lirch_constants.h)
 	for (auto &p : preloads)
 	{
-		vm.push_back(plugin_adder::create(string(p.name), string(p.filename)));
+		plugins.push_back(make_pair<string, string>(p.name, p.filename));
 	}
 
-	// Load plugins specified on command line
-	// TODO can we parse the args with QApplication?
-	if (argc > 1) {
-		int i = 1, j = 0;
-		// The first argument might specify verbose mode
-		if (argv[i] == string("-v") || argv[i] == string("--verbose")) {
-			cerr << "Lirch " << LIRCH_VERSION_STRING << " Core (" << LIRCH_BUILD_HASH << ")" << endl;
-			cerr << "preloads: ";
-			for (auto &p : preloads) {
-				cerr << p.name << " ";
-			}
-			cerr << ";" << endl;
-			for (auto &p : preloads) {
-				cerr << ++j << ") " << p.filename << endl;
-			}
-			verbose = true;
-			++i;
+	// TODO figure out what happens to unparsed args
+	cerr << "Before parse: ";
+	for (int i = 0; i < argc; ++i) {
+		cerr << "'" << argv[i] << "' ";
+	}
+	cerr << "(end)" << endl;
+	parse_args(argc, argv);
+	verbose = session.verbose;
+	cerr << "After parse: ";
+	for (int i = 0; i < argc; ++i) {
+		cerr << "'" << argv[i] << "' ";
+	}
+	cerr << "(end)" << endl;
+
+	// Handle a request to display these
+	if (session.list_preloads) {
+		for (auto &p : plugins) {
+			cerr << p.first << ": " << p.second << endl;
 		}
-		// The rest are plugin (name, filename) pairs (need two at a time)
-		string name, filename;
-		while (i + 1 < argc) {
-			name = argv[i++];
-			filename = argv[i++];
-			vm.push_back(plugin_adder::create(name, filename));
+	}
+
+	// Honor a request to ignore these
+	if (session.no_preloads) {
+		plugins.clear();
+	}
+
+	// Add in plugins specified by the command line
+	auto pit = session.plugins.begin();
+	auto pitend = session.plugins.end();
+	pair<string, string> p;
+	while (pit != pitend) {
+		p.first = *pit; ++pit;
+		if (pit == pitend) {
+			break;
 		}
+		p.second = *pit; ++pit;
+		plugins.push_back(p);
 	}
 
 	// Run the core in a separate thread
-	thread core_thread(run_core, vm);
+	vector<message> add_messages;
+	for (auto &p : plugins) {
+		add_messages.push_back(plugin_adder::create(p.first, p.second));
+	}
+	thread core_thread(run_core, add_messages);
 	core_thread.detach();
+
 	// When the event loop terminates, so will we
 	return lirch.exec();
 }
